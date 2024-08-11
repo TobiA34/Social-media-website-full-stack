413dce5616e9e2cf0cecc5550be2a3be0810d01d
“TobiA34” <tobiadegoroye49@gmail.com>
2024-08-11 20:35:14 +0100

    Added categories, New nav bar with bootstrap

---
+++ backend/models/Categories.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Categories = sequelize.define("Categories", {
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      distinct:true // add this distinct here
+     },
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "Users",
+        targetKey: "id",
+      },
+    },
+  });
+  
+  Categories.associate = (models) => {
+
+ 
+
+     Categories.belongsTo(models.Users, { foreignKey: "UserId" });
+      Categories.belongsTo(models.Posts, { foreignKey: "CategoryId" });
+ Categories.hasMany(models.Posts, { foreignKey: "CategoryId" });
+  };
+   
+  return Categories;
+};
